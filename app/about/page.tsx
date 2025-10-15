b68cc3c2d93edaa4d8d2834729bf9c2cd0c910b5
Letam <davidsonletam@gmail.com>
2025-10-15 09:57:55 +0100

    FEAT: ABOUT IMAGES

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,6 +20,28 @@ const page = () => {
 						Get in Touch
 					</button>
 				</Link>
+				<div className="flex items-center justify-center space-x-6">
+					<div className="h-[160px] w-[160px] sm:w-[320px] sm:h-[320px] bg-[#FFD8B5] rounded-tl-[100px] sm:rounded-tl-[200px] rounded-br-[100px] sm:rounded-br-[200px] flex items-end overflow-hidden">
+						<div className="relative h-[150px] sm:h-[300px] w-[150px] sm:w-[500px]">
+							<Image
+								alt=""
+								src={"/little-girl-with-backpack-studio 1.svg"}
+								fill
+								className="object-cover"
+							/>
+						</div>
+					</div>
+					<div className="h-[160px] w-[160px] sm:w-[320px] sm:h-[320px] bg-[#FFF5BA] rounded-bl-[100px] rounded-tr-[100px] sm:rounded-bl-[200px] sm:rounded-tr-[200px] flex items-end overflow-hidden">
+						<div className="relative h-[150px] sm:h-[300px] w-[150px] sm:w-[500px]">
+							<Image
+								alt=""
+								src={"/little-boy-with-notebooks 1.svg"}
+								fill
+								className="object-cover"
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div className="w-full">
 				{/* <div className="relative h-[75px] sm:hidden block">
@@ -74,7 +96,7 @@ const page = () => {
                 <Image src={"/amp-bottom.svg"} alt="t`op" fill className="" />
               </div> */}
 			</div>
-			<div className="flex flex-col items-center justify-center space-y-5 px-3">
+			<div className="flex flex-col items-center justify-center space-y-5 px-3 pb-40">
 				<h1 className="text-dark howdybun font-normal text-[32px] sm:text-[64px] text-center uppercase w-full max-w-[366px] sm:max-w-full">
 					<span className="text-light">Our Teaching Philosophy</span>
 				</h1>
@@ -85,14 +107,14 @@ const page = () => {
 					while developing strong values, critical thinking, and a lifelong love
 					for learning.
 				</p>
-				<div className="relative">
+				{/* <div className="h-40 w-40">
 					<Image
 						alt=""
 						fill
 						src="/teacher.svg"
-						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						sizes="(max-width: 768px) 10vw, (max-width: 1200px) 50vw, 33vw"
 					/>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
